51f7a3ed6ced4a0c053edcf946b0b12af1556d8c
Berenice Lopez Gonzalez <8122110004@utchetumal.edu.mx>
2024-02-08 09:57:15 -0500

    Commit actualizado

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>
+        He instalado Vue3 en Laravel 10 :)
+    </h1>
+    <br>
+    <h1>
+    Actividad 4 de Aplicaciones web para I4.0
+    </h1>
+</template>
---
+++ resources/js/main.js
@@ -0,0 +1,7 @@
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
